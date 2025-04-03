2a0e1cfb95f00238cd3d62cf173d2a67b7523fd5
Stefhany Ortega <svortega.9941@gunicesmag.edu.co>
2025-04-02 21:59:04 -0500

    postman

---
+++ src/signup.php
@@ -0,0 +1,27 @@
+<?php 
+    $host     = "localhost";
+    $port     = "5432";
+    $dbname   = "petstore";
+    $user     = "postgres";
+    $password = "postgres";
+
+    $data_connection ="
+        host=$host
+        port=$port  
+        dbname=$dbname
+        user=$user
+        password=$password"
+        ;
+    $conn= pg_connect($data_connection);
+
+    if(!$conn){
+        echo "Connection error"; }
+        else {
+            echo" Success !!!";
+
+       }
+
+       pg_close($conn);
+
+    
+?>
