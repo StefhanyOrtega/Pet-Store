0d050cc77f1bc7f7d41be5e3df58303938baca89
Stefhany Ortega <svortega.9941@gunicesmag.edu.co>
2025-04-03 20:10:23 -0500

    validacion email

--- src/signup.php
+++ src/signup.php
@@ -1,27 +1,30 @@
-<?php 
-    $host     = "localhost";
-    $port     = "5432";
-    $dbname   = "petstore";
-    $user     = "postgres";
-    $password = "postgres";
+<?php
 
-    $data_connection ="
-        host=$host
-        port=$port  
-        dbname=$dbname
-        user=$user
-        password=$password"
-        ;
-    $conn= pg_connect($data_connection);
+include('config/database.php');
 
-    if(!$conn){
-        echo "Connection error"; }
-        else {
-            echo" Success !!!";
+$fname=$_POST['f_name'];
+$lname=$_POST['l_name'];
+$email=$_POST['e_mail'];
+$passw=$_POST['p_assw'];
 
-       }
+$sql_validate_email= "
+select count (id) as total from users where email='$email'and status =true 
+";
 
-       pg_close($conn);
+$ans= pg_query ($conn,$sql_validate_email);
+if ($ans){
+    $row = pg_fetch_assoc($ans);
+if($row ['total'] > 0){
+    echo"user already exist";
+}else{
+    $sql= "INSERT INTO users(
+        firstname,lastname,email,password)
+        values ('$fname','$lname','$email','$passw')";
+        $ans= pg_query ($conn,$sql);
+        if ($ans){
+        echo "user has been created succesfully";
+        }else {
+        echo "error";
+        }}}
 
-    
 ?>
